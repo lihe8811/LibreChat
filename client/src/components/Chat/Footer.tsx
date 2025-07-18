6928fd4fe988c3e500360e59259ca9c7b5f9f36e
ShengQiang <sophontec@gmail.com>
2025-07-18 14:07:22 +0800

    refactor: 1. change the librechat link to qingfan.ai; 2. change the footer

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -37,9 +37,9 @@ export default function Footer({ className }: { className?: string }) {
   const mainContentParts = (
     typeof config?.customFooter === 'string'
       ? config.customFooter
-      : '[LibreChat ' +
+      : '[QingFan AI ' +
         Constants.VERSION +
-        '](https://librechat.ai) - ' +
+        '](https://sophontec.github.io/) - ' +
         localize('com_ui_latest_footer')
   ).split('|');
 
